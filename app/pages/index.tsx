48be4f2d3a946e92374f0a2422b4b4553b999a36
Cedric Chee <cedric@invictusbyte.com>
2020-07-03 00:18:30 +0800

    Update homepage for voting app

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,7 +3,7 @@ import { Head, Link } from "blitz"
 const Home = () => (
   <div className="container">
     <Head>
-      <title>myapp</title>
+      <title>Voting app</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
@@ -13,17 +13,22 @@ const Home = () => (
       </div>
       <p>1. Run this command in your terminal:</p>
       <pre>
-        <code>blitz generate all project name:string</code>
+        <code>
+          npx blitz generate all question text hasMany:choices
+          <br />
+          <br />
+          npx blitz generate resource choice text votes:int:default[0] belongsTo:question
+        </code>
       </pre>
       <p>2. Then run this command:</p>
       <pre>
-        <code>blitz db migrate</code>
+        <code>npx blitz db migrate</code>
       </pre>
 
       <p>
         3. Go to{" "}
-        <Link href="/projects">
-          <a>/projects</a>
+        <Link href="/questions">
+          <a>/questions</a>
         </Link>
       </p>
       <div className="buttons">
